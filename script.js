aa2d8b6ec6335d20cfeec0855ccc3427c7e0c24d
SeanPayneArchDesign <74615707+SeanPayneArchDesign@users.noreply.github.com>
2021-06-13 20:11:44 +0100

    updated to obj loader source

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.119.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.119.0/examples/jsm/loaders/GLTFLoader.js'
+import { OBJLoader } from 'https://unpkg.com/three@0.119.0/examples/jsm/loaders/OBJLoader.js'
 
 let renderer = null;
 let scene = null;
